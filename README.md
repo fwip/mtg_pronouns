2839cefa800aeab5c113922a4d3ca2302a7b9bd2
Jemma Nelson <jemma.s.nelson@gmail.com>
2014-01-05 00:57:34 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# An experiment with MTG and pronouns
+
+There's been a bit of discussion regarding "his or her" vs. "their" in rules text of Magic: The Gathering cards. This project is intended to check out the feasibility of a simple replacement.
---
+++ to_they.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/ruby
+
+require 'yaml'
+require 'optparse'
+
+options = {}
+
+@replacements = {
+  ' his or her '  => ' their ',
+  ' he or she '   => ' they ',
+  ' that player ' => ' they ',
+  ' His or her '  => ' Their ',
+  ' He or she '   => ' They ',
+  ' That player ' => ' They '
+}
+
+def rephraseCard(card)
+  changed = false
+  rules = card['rules']
+  return card, false if rules.nil?
+  @replacements.each_pair do |search, replace|
+    localchange = rules.gsub! search, replace
+    changed = changed || ! localchange.nil?
+  end
+  card['rules'] = rules
+  return card, changed
+end
+
+def card_to_s(card)
+    text =  "#{card['name']} #{card['cost']}\n"
+    text += "---------------\n"
+    text += card['rules']
+    text += "\n\n"
+    return text
+end
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: to_they.rb <file> [options]"
+
+  opts.on("-c", "--changed", "Only print changed") do |c|
+    options[:changed] = c
+  end
+
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+end.parse!
+
+cardfile = ARGV.first
+cards = YAML.load_file cardfile
+
+cards.each do |card|
+  card, changed = rephraseCard card
+  if !options[:changed] or changed
+    puts card_to_s card
+  end
+end
